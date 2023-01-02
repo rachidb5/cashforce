5acf58732334fec8d61af9d2ab0f2ee13ef75276
rachidb5 <jordan.rachid@gmail.com>
2023-01-01 21:32:54 -0300

    fatora tabela

--- cashforce-front/src/components/Table.vue
+++ cashforce-front/src/components/Table.vue
@@ -7,7 +7,7 @@
       </v-row>
       <span>Visualize as notas fiscais que você tem</span>
     </v-column>
-    <v-table>
+    <table>
       <thead>
         <tr>
           <th class="text-left">NOTA FISCAL</th>
@@ -19,28 +19,24 @@
           <th class="text-left">AÇÕES</th>
         </tr>
       </thead>
-    </v-table>
-    <div class="data-card" v-for="i in list">
-      <v-table>
-        <tbody>
-          <tr>
-            <td>{{ i.orderNfId }}</td>
-            <td>{{ i.buyers.name }}</td>
-            <td>{{ i.providers.name }}</td>
-            <td>{{ dateFormat(i.createdAt) }}</td>
-            <td class="green-text">
-              R$ {{ (i.value / 100).toFixed(2).toString().replace(".", ",") }}
-            </td>
-            <td class="green-text">{{ statusDefine(i.orderStatusBuyer) }}</td>
-            <td>
-              <button elevation="2" @click="openModal(i.providerId)">
-                Dados do cedente
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </v-table>
-    </div>
+      <tbody>
+        <tr v-for="i in list">
+          <td>{{ i.orderNfId }}</td>
+          <td>{{ i.buyers.name }}</td>
+          <td>{{ i.providers.name }}</td>
+          <td>{{ dateFormat(i.createdAt) }}</td>
+          <td class="green-text">
+            R$ {{ (i.value / 100).toFixed(2).toString().replace(".", ",") }}
+          </td>
+          <td class="green-text">{{ statusList[i.orderStatusBuyer] }}</td>
+          <td>
+            <button elevation="2" @click="openModal(i.providerId)">
+              Dados do cedente
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <v-row justify="center">
       <v-dialog v-model="dialog" persistent max-width="690">
         <template v-slot:activator="{ on, attrs }"> </template>
@@ -100,10 +96,26 @@
   </div>
 </template>
 <style scoped>
-.v-table .v-table__wrapper > table > thead > tr:last-child > th {
-  border-bottom: none;
+table {
+  border-collapse: separate !important;
+  border-spacing: 0 1rem;
 }
-.v-card{
+td {
+  padding: .5rem;
+  font-size: 12px !important;
+  text-align: center;
+  border-top: 1px solid #dfe2eb;
+  border-bottom: 1px solid #dfe2eb;
+}
+td:first-of-type {
+  border-radius: .5rem 0 0 .5rem;
+  border-left: 1px solid #dfe2eb;
+}
+td:last-of-type {
+  border-radius: 0 .5rem .5rem 0;
+  border-right: 1px solid #dfe2eb;
+}
+.v-card {
   padding: 1rem;
 }
 .green-text {
@@ -121,20 +133,10 @@ button:hover {
   padding: 5% 10% 5% 10%;
   border-radius: 25px;
 }
-td {
-  padding: 0 !important;
-  font-size: 12px !important;
-  width: 140px;
-  text-align: center;
-}
 th {
   padding: 0 !important;
   text-align: center !important;
-  width: 140px;
-}
-tr {
-  margin: 2%;
-  border: 1px solid black;
+  width: 12%;
 }
 .main {
   margin-top: 5%;
@@ -155,11 +157,6 @@ h3 {
   color: #021b51;
   line-height: 28px;
 }
-.data-card {
-  border: 1px solid #dfe2eb;
-  border-radius: 6px;
-  margin-bottom: 1%;
-}
 span {
   color: #727d94;
   margin-top: 1rem !important;
@@ -170,10 +167,6 @@ span {
   font-size: 12px;
   font-weight: 300;
 }
-.body-tr {
-  border: 2px solid black !important;
-  margin-bottom: 3%;
-}
 </style>
 <script>
 import axios from "axios";
@@ -182,7 +175,18 @@ export default {
     return {
       list: [],
       dialog: false,
-      provider:{}
+      provider: {},
+      statusList: [
+        "Pendente de confirmação",
+        "Pedido confirmado",
+        "Não reconhece o pedido",
+        "Mercadoria não recebida",
+        "Recebida com avaria",
+        "Devolvida",
+        "Recebida com devolução parcial",
+        "Recebida e confirmada",
+        "Pagamento Autorizado",
+      ],
     };
   },
   methods: {
@@ -201,30 +205,16 @@ export default {
         date.slice(8, 10) + "/" + date.slice(5, 7) + "/" + date.slice(0, 4)
       );
     },
-    statusDefine(s) {
-      let statusList = [
-        "Pendente de confirmação",
-        "Pedido confirmado",
-        "Não reconhece o pedido",
-        "Mercadoria não recebida",
-        "Recebida com avaria",
-        "Devolvida",
-        "Recebida com devolução parcial",
-        "Recebida e confirmada",
-        "Pagamento Autorizado",
-      ];
-      return statusList[s];
-    },
-    async openModal(id){
-      this.dialog = true
+    async openModal(id) {
+      this.dialog = true;
       await axios
         .get(`https://cashforce-back.fly.dev/provider/${id}`)
         .then((response) => {
           console.log(response.data);
-          this.provider = response.data
+          this.provider = response.data;
         })
         .catch((e) => console.log(e.response));
-    }
+    },
   },
   beforeMount() {
     this.getList();
