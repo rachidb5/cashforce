1089cf8def46042c56b988c64932930548d9a680
rachidb5 <jordan.rachid@gmail.com>
2023-01-04 11:36:15 -0300

    container do banco de dados

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -3,28 +3,28 @@ require('dotenv').config();
 module.exports = {
     development: {
         username: 'root',
-        password: '123321',
+        password: 'root',
         database: 'cashforce',
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 3306,
         dialect: 'mysql',
         logging: true
     },
     test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PWD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        username: 'root',
+        password: 'root',
+        database: 'cashforce',
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
         logging: true
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PWD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT
+        username: 'root',
+        password: 'root',
+        database: 'cashforce',
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
     }
 };
